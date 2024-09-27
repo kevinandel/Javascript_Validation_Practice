fa34f7ff17ebce9859e6566a73e14233e81f1095
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-27 11:58:06 +0530

    Practicing simple javascript validation

---
+++ btnscript.js
@@ -0,0 +1,9 @@
+
+// var button = document.getElementById("btn");
+// var hiii = document.getElementById("hii");
+// button.addEventListener("click",Text);
+// function Text(){
+//   button.style.backgroundColor="red";
+//   document.getElementById("hii").innerHTML  = "poda";
+//   hiii.style.fontSize='100px';
+// }
---
+++ validation.js
@@ -0,0 +1,4 @@
+var button = document.getElementById("btn");
+function changeColor(){
+  button.style.backgroundColor="red";
+}
